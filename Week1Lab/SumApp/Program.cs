a999215fcf7d1e65cbce612a47f0d0a6b41ba79f
rtiare <ttorosa55@gmail.com>
2021-06-05 19:52:02 -0700

    Implemented do-while loop for menu system

--- Week1Lab/SumApp/Program.cs
+++ Week1Lab/SumApp/Program.cs
@@ -6,24 +6,30 @@ namespace SumApp
     {
         static void Main(string[] args)
         {
-            var rand = new Random();
+            string? userInputRepeat = default;
+            do {
+                var rand = new Random();
 
-            //create random variables
-            var a = rand.Next(11);
-            var b = rand.Next(11);
-            Console.Write($"{a} + {b} = ");
+                //create random variables
+                var a = rand.Next(11);
+                var b = rand.Next(11);
+                Console.Write($"{a} + {b} = ");
 
-            //read user input
-            var userInput = Console.ReadLine();
+                //read user input
+                var userInput = Console.ReadLine();
 
-            //determine is user input is wrong or correct
-            if ((a + b) == int.Parse(userInput))
-            {
-                Console.WriteLine("Your answer is correct.");
-            }
-            else {
-                Console.WriteLine($"Your answer is incorrect. The correct answer is {a+b}.");
-            }
+                //determine is user input is wrong or correct
+                if ((a + b) == int.Parse(userInput))
+                {
+                    Console.WriteLine("Your answer is correct.");
+                }
+                else
+                {
+                    Console.WriteLine($"Your answer is incorrect. The correct answer is {a + b}.");
+                }
+                Console.Write("Do you want to try again? ");
+                userInputRepeat = Console.ReadLine();
+            } while (userInputRepeat == "y" || userInputRepeat == "Y");
         }
     }
 }
