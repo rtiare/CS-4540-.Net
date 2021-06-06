069d5dbdc8fca8332d9543bcbf9bb07ca6bef604
rtiare <ttorosa55@gmail.com>
2021-06-06 00:30:37 -0700

    Compared arrays

--- Week1Lab/DetermineSortApp/Program.cs
+++ Week1Lab/DetermineSortApp/Program.cs
@@ -9,9 +9,51 @@ namespace DetermineSortApp
 
             //enter userInput
             Console.Write("Enter list: ");
-            string userInput = Console.ReadLine();
+            string[] userInput = Console.ReadLine().Split();
 
+            //create int array to store string array
+            int[] userInt = new int [userInput.Length];
+            for (int i = 0; i < userInput.Length; i++) {
+                userInt[i] = int.Parse(userInput[i]);
+            }
 
+            //store array in ascending and descending array
+            int[] ascending = new int[userInt.Length];
+            int[] descending = new int[userInt.Length];
+            userInt.CopyTo(ascending, 0);
+            userInt.CopyTo(descending, 0);
+
+            //reverse and sort the new arrays
+            Array.Sort(ascending);
+            Array.Sort(descending);
+            Array.Reverse(descending);
+
+            //boolean to determine sort
+            Boolean isSorted = true;
+
+            //compare ascending and descending
+            for (int i = 0; i < userInt.Length; i++) {
+                if (userInt[i] != ascending[i]) {
+                    isSorted = false;
+                }
+            }
+            isSorted = true;
+            for (int i = 0; i < userInt.Length; i++) {
+                if (userInt[i] != descending[i]) {
+                    isSorted = false;
+                }
+            }
+
+            //write statement based on boolean
+            if (isSorted == false)
+            {
+                Console.WriteLine("The list is not sorted");
+            }
+            else {
+                Console.WriteLine("The list is sorted");
+            }
+  
+ 
         }
     }
 }
