e067885e6dc92699961f65f0d71b562971106a4f
rtiare <ttorosa55@gmail.com>
2021-06-05 19:17:34 -0700

    Determine if iuser input is wrong

--- Week1Lab/SumApp/Program.cs
+++ Week1Lab/SumApp/Program.cs
@@ -7,9 +7,23 @@ namespace SumApp
         static void Main(string[] args)
         {
             var rand = new Random();
+
+            //create random variables
             var a = rand.Next(11);
             var b = rand.Next(11);
-            Console.WriteLine($"{a} + {b}");
+            Console.Write($"{a} + {b} = ");
+
+            //read user input
+            var userInput = Console.ReadLine();
+
+            //determine is user input is wrong or correct
+            if ((a + b) == int.Parse(userInput))
+            {
+                Console.WriteLine("Your answer is correct.");
+            }
+            else {
+                Console.WriteLine($"Your answer is incorrect. The correct answer is {a+b}.");
+            }
         }
     }
 }
