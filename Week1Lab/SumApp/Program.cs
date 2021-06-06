a279dd8038fa1f5ffcdf6a3615f9bbb632c2c54a
rtiare <ttorosa55@gmail.com>
2021-06-05 18:35:38 -0700

    Add project files.

---
+++ Week1Lab/SumApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
