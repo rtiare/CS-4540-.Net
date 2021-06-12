566dbfde5f9e2ea79de57ab56685c826a5e8774c
rtiare <ttorosa55@gmail.com>
2021-06-12 02:01:06 -0700

    Commit vaccine app

---
+++ VaccineApp/VaccineApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VaccineApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
