b90f1fdce68b9b1472773fedfab5996c4cfc595b
rtiare <ttorosa55@gmail.com>
2021-06-27 01:00:13 -0700

    created week 4 laab

---
+++ Week4Lab/Week4Lab/Company.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Week4Lab
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateHired { get; set; }
+        public int? SupervisorId { get; set; }
+
+        public Employee() { }
+
+        public Employee(int id, string firstName, string lastName, DateTime dateHired, int? supervisorId = null)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            DateHired = dateHired;
+            SupervisorId = supervisorId;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {FirstName} {LastName}, {DateHired:d}, {SupervisorId}";
+        }
+    }
+
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Employee Leader { get; set; }
+        public List<Employee> Members { get; set; }
+
+        public Project()
+        {
+            Members = new List<Employee>();
+        }
+
+        public Project(int id, string name) : this()
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id} {Name}";
+        }
+    }
+
+    public class Company
+    {
+        public List<Employee> Employees { get; set; }
+        public List<Project> Projects { get; set; }
+
+        public Company()
+        {
+            var john = new Employee(1, "John", "Doe", new DateTime(2015, 1, 10));
+            var jane = new Employee(2, "Jane", "Doe", new DateTime(2015, 2, 20), 1);
+            var tom = new Employee(3, "Tom", "Smith", new DateTime(2016, 6, 19), 2);
+            var bob = new Employee(4, "Bob", "Lee", new DateTime(2016, 6, 20), 2);
+
+            Employees = new List<Employee>();
+            Employees.Add(john);
+            Employees.Add(jane);
+            Employees.Add(tom);
+            Employees.Add(bob);
+
+            var firestone = new Project(1, "Firestone")
+            {
+                Leader = john,
+                Members = new List<Employee> { john }
+            };
+            var blue = new Project(2, "Blue")
+            {
+                Leader = jane,
+                Members = new List<Employee> { john, jane }
+            };
+
+            Projects = new List<Project>();
+            Projects.Add(firestone);
+            Projects.Add(blue);
+        }
+    }
+}
---
+++ Week4Lab/Week4Lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Week4Lab
+{
+    class Program
+    {
+        static void Print<T>(string label, IEnumerable<T> results)
+        {
+            Console.WriteLine(label);
+            foreach (var result in results)
+                Console.WriteLine("\t{0}", result);
+        }
+
+        static void Main(string[] args)
+        {
+            Company c = new Company();
+
+            // 1. List the names of the employees who do not have a supervisor.
+            // The results should combine FirstName and LastName into one string.
+
+            // 2. List the last names of the employees whose last name starts with D.
+            // The results should be listed in alphabetic order without duplicates.
+
+            // 3. List the names of the employees who are on the project Blue.
+            // The results should combine FirstName and LastName into one string.
+
+            // 4. Find Jane Doe's subordinates, i.e. the employees who are supervised by Jane Doe.
+
+            // 5. Find the employee(s) who were hired in 2015 and worked on the project Blue.
+        }
+    }
+}
