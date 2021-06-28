dfd47cf4bb3ffa28cd3508dfd998b6d547cc6c0f
rtiare <ttorosa55@gmail.com>
2021-06-27 19:25:32 -0700

    implemented LINQ methods

--- Week4Lab/Week4Lab/Program.cs
+++ Week4Lab/Week4Lab/Program.cs
@@ -20,7 +20,9 @@ namespace Week4Lab
             // 1. List the names of the employees who do not have a supervisor.
             // The results should combine FirstName and LastName into one string.
 
-            var r1 = from e in c.Employees where e.SupervisorId is null select (e.FirstName , e.LastName);
+            var r1 = from e in c.Employees 
+                     where e.SupervisorId is null 
+                     select (e.FirstName , e.LastName);
             Print("Q1 (query)", r1);
 
             // 2. List the last names of the employees whose last name starts with D.
@@ -29,15 +31,35 @@ namespace Week4Lab
             var r2 = c.Employees
                 .Where(e => e.LastName.StartsWith("D"))
                 .OrderBy(e => e.LastName)
-                .Select(e => new { e.FirstName, e.LastName });
+                .Select(e => e.LastName);
             Print("Q2 (query)", r2);
 
             // 3. List the names of the employees who are on the project Blue.
             // The results should combine FirstName and LastName into one string.
 
+            var r3 = (from p in c.Projects 
+                      where p.Name == "Blue"
+                      select p.Members).Single();
+            Print("Q3 (query)", r3);
+
             // 4. Find Jane Doe's subordinates, i.e. the employees who are supervised by Jane Doe.
 
+            var r4 = from e1 in c.Employees
+                     join e2 in c.Employees
+                     on e1.SupervisorId equals e2.Id
+                     where e2.FirstName == "Jane" && e2.LastName == "Doe"
+                     select e1;
+            Print("Q4 (query", r4);
+
             // 5. Find the employee(s) who were hired in 2015 and worked on the project Blue.
+
+            var r5 = (from e in c.Employees
+                      where e.DateHired.Year == 2015
+                      select e)
+                      ;
+
+            Print("Q5 (query)", r5);
+               
         }
     }
 }
