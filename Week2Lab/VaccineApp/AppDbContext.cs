52af10af7b111440d802d59f2c213f2f370cf467
rtiare <ttorosa55@gmail.com>
2021-07-10 18:45:58 -0700

    Connected to sql server

---
+++ Week2Lab/VaccineApp/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VaccineApp.Model;
+
+
+namespace VaccineApp
+{
+    class AppDbContext : DbContext
+    {
+        private readonly string ConnectionString =
+            "Server=cs3.calstatela.edu;Database=cs4540stu30;User ID = cs4540stu30; Password=qKNZQtCkG5Qf";
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+            optionsBuilder.UseSqlServer(ConnectionString).UseLazyLoadingProxies();
+        }
+
+        public DbSet<VaccineModel> Vaccines { get; set; } 
+    }
+}
