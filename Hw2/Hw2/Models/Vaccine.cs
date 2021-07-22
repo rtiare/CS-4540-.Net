4991eecc92a504ad54c1a7b5c75c6c6bfb373db1
rtiare <ttorosa55@gmail.com>
2021-07-22 03:20:14 -0700

    created vaccine model

---
+++ Hw2/Hw2/Models/Vaccine.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw2.Models
+{
+    public class Vaccine
+    {
+        //setters and getters
+        public int Id { get; set; }
+        public string VaccineName { get; set; }
+        public int? DoseRequired { get; set; }
+        public int? DaysBtwDose { get; set; }
+        public int? TotalDose { get; set; }
+
+        public Vaccine() { }
+
+        public Vaccine(int id, string name, int? dose, int? days, int? total ) {
+            Id = id;
+            VaccineName = name;
+            DoseRequired = dose;
+            DaysBtwDose = days;
+            TotalDose = total;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0,-3}{1,-23}{2,-18}{3,-21}{4,-12}\n",
+                    $"{Id})",
+                    $"{VaccineName}",
+                    $"{DoseRequired}",
+                    $"{DaysBtwDose}",
+                    $"{TotalDose}");
+        }
+
+    }
+}
