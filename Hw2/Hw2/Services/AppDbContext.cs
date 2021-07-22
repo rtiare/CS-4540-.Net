63455616af4b4e003d7332210a2e536a50480ce7
rtiare <ttorosa55@gmail.com>
2021-07-22 04:04:48 -0700

    Created DbContext class

---
+++ Hw2/Hw2/Services/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Hw2.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw2.Services
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Vaccine> Vaccines { get; set } 
+        //"ConnectionStrings": {"DefaultConnection": "Server=...;Database=...;User ID=...;Password=...;"},
+    }
+}
