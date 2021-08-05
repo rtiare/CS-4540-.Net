7c2e066a8badc468470cd4d43f1d9949834b2581
rtiare <ttorosa55@gmail.com>
2021-08-04 18:44:48 -0700

    Created patient model class

---
+++ Hw2/Hw2/Controllers/PatientController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw2.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hw2/Hw2/Models/Patient.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hw2.Models
+{
+    public class Patient
+    {
+        //setters and getters
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public String Vaccine { get; set; }
+
+        public Vaccine vaccineSelected { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime FirstDose { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime SecondDose { get; set; }
+
+        public Patient() { }
+
+        public Patient(string name, string vaccine, DateTime firstDose, DateTime secondDose)
+        {
+            Name = name;
+            Vaccine = vaccine;
+            FirstDose = firstDose;
+            SecondDose = secondDose;
+
+        }
+    }
+}
